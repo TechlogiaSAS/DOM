d4c62e5079079ba0cb22fb0f651043ecdb28f4e9
FelipeRLeon <feliiperestrepoleon@gmail.com>
2023-06-14 10:40:28 -0500

    Test1

--- js/app.js
+++ js/app.js
@@ -8,7 +8,6 @@ titulo.textContent = "Frontend Avanzado";
 link.href = "https://www.discogs.com/es/release/12790214-Mala-Fama-Ritmo-Sustancia"
 
 const img1 = document.getElementById("img1");
-
 const img2 = document.getElementById("img2");
 
 //Obtiene el valor de un atributo
@@ -16,7 +15,6 @@ const src1 = img1.getAttribute("src");
 console.log(src1);
 
 //Modifica el atributo de un elemento
-
 img2.setAttribute("src", src1);
 
 
